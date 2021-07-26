2df6c11d407600e0219ac97f3eecb3dcc4968207
Carl Haricombe <73272379+J3diHunt3r@users.noreply.github.com>
2021-07-27 00:16:36 +0800

    Add files via upload

---
+++ src/Components/TodoList.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { TodoListItem } from "./TodoListItems";
+
+interface TodoListProps {
+  todos: Array<Todo>;
+  toggleTodo: ToggleTodo;
+  deleteTodo: DeleteTodo;
+  editTodo: EditTodo;
+  getEditText: GetEditText;
+  saveEditedTodo: SaveEditedTodo;
+  currentFilter: CurrentFilter;
+}
+
+//displaying all prop functions and labels 
+export const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  toggleTodo,
+  deleteTodo,
+  editTodo,
+  getEditText,
+  saveEditedTodo
+}) => {
+  return (
+    <ul>
+      {todos.map((todo, i) => {
+        return (
+          <TodoListItem
+            key={i}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            saveEditedTodo={saveEditedTodo}
+            getEditText={getEditText}
+          />
+        );
+      })}
+    </ul>
+  );
+};
